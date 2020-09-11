28025913345b79190f2f074aaec63ea208e10df6
KerabatOnline <71099103+KerabatOnline@users.noreply.github.com>
2020-09-11 20:51:21 +0800

    Update time.py

--- userbot/plugins/time.py
+++ userbot/plugins/time.py
@@ -15,7 +15,7 @@ FONT_FILE_TO_USE = "/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf"
 async def _(event):
     if event.fwd_from:
         return
-    current_time = datetime.now().strftime("• \nSAM RAMADHAN\n Lokasi: Indonesia \n  Waktu: %H:%M:%S \n  Tanggal: %d.%m.%y \n➡➡➡➡➡➡➡")
+    current_time = datetime.now().strftime("SAM RAMADHAN\n \nKota Makassar\n  Waktu: %H:%M:%S \n  Tanggal: %d.%m.%y \n➡➡➡➡➡➡➡")
     start = datetime.now()
     input_str = event.pattern_match.group(1)
     reply_msg_id = event.message.id
